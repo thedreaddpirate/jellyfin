7b278db1ed2951751e2c25aca96c9b354b8171d2
Shadowghost <Ghost_of_Stone@web.de>
2026-09-08 09:47:53 +0200

    Delete the stale WAL when restoring a pre-migration backup

--- src/Jellyfin.Database/Jellyfin.Database.Providers.Sqlite/SqliteDatabaseProvider.cs
+++ src/Jellyfin.Database/Jellyfin.Database.Providers.Sqlite/SqliteDatabaseProvider.cs
@@ -259,6 +259,9 @@ public sealed class SqliteDatabaseProvider : IJellyfinDatabaseProvider
 
         File.Copy(backupFile, path, true);
 
+        File.Delete(path + "-wal");
+        File.Delete(path + "-shm");
+
         return Task.CompletedTask;
     }
 
