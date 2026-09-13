1a66bb7293a2091961db7b67775cd1917d10351a
Shadowghost <Ghost_of_Stone@web.de>
2026-09-13 23:43:17 +0200

    Stop resolving items with every field where only stored columns are read

--- MediaBrowser.Controller/Entities/Movies/BoxSet.cs
+++ MediaBrowser.Controller/Entities/Movies/BoxSet.cs
@@ -173,7 +173,7 @@ namespace MediaBrowser.Controller.Entities.Movies
             var libraryFolderIds = LibraryFolderIds;
             if (libraryFolderIds is null)
             {
-                linkedItems = GetLinkedChildren();
+                linkedItems = GetLinkedChildren(DtoOptions.StoredColumnsOnly);
                 libraryFolderIds = GetLibraryFolderIds(linkedItems);
             }
 
@@ -192,7 +192,7 @@ namespace MediaBrowser.Controller.Entities.Movies
             // If user has parental controls, hide the BoxSet when all children are restricted
             if (user.MaxParentalRatingScore.HasValue)
             {
-                linkedItems ??= GetLinkedChildren();
+                linkedItems ??= GetLinkedChildren(DtoOptions.StoredColumnsOnly);
                 if (linkedItems.Count > 0 && linkedItems.All(child => !child.IsParentalAllowed(user, true)))
                 {
                     return false;
@@ -249,7 +249,7 @@ namespace MediaBrowser.Controller.Entities.Movies
 
         public Guid[] GetLibraryFolderIds()
         {
-            return GetLibraryFolderIds(GetLinkedChildren());
+            return GetLibraryFolderIds(GetLinkedChildren(DtoOptions.StoredColumnsOnly));
         }
 
         private Guid[] GetLibraryFolderIds(IEnumerable<BaseItem> linkedChildren)
